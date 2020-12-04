96b419d17db829ca1fca98c39561970afc932e27
s-macias <1552@holbertonschool.com>
2020-12-04 03:42:29 +0000

    task 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" concatenates two arrays of ints/floats """
+
+
+def cat_arrays(arr1, arr2):
+    """ concatenates two arrays """
+    concat = []
+    concat += arr1
+    concat += arr2
+    return concat
