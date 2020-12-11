eae071e1ee1bcadce1c5f7d7d6586db983d5ae5d
s-macias <1552@holbertonschool.com>
2020-12-11 04:43:44 +0000

    task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/ env python3
+""" calculates the integral  of a polynomial """
+
+
+def poly_integral(poly, C=0):
+    """ calculates the integral of a polynomial """
+    if type(poly) is not list or poly is None or type(C) is not int:
+        return None
+    integral = []
+    i = 0
+    for coef in poly:
+        item = poly[i] / (i + 1)
+        integral.append(item)
+        i += 1
+    return integral
