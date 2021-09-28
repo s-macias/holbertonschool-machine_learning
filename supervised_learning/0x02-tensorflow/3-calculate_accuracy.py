330a43a78aaf46650afd2c856eb64661d6559f84
s-macias <1552@holbertonschool.com>
2021-09-28 01:48:16 +0000

    task 3

---
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Module with calculate_accuracy function """
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Function that calculates the accuracy of a prediction
+    Arguments:
+    y is a placeholder for the labels of the input data
+    y_pred is a tensor containing the network’s predictions
+    Returns:
+    a tensor containing the decimal accuracy of the prediction
+    hint: accuracy = correct_predictions / all_predictions
+    """
+    correct_predictions = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    return tf.reduce_mean(tf.cast(correct_prediction, 'float32'))
