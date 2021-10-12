18a84e39465eb46f866fee71f40d295f5e0f7a36
s-macias <1552@holbertonschool.com>
2021-10-12 21:26:46 +0000

    fixing pycodestyle

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -35,7 +35,6 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
-    
     if padding == 'same':
         ph = int(((h_prev - 1) * sh - h_prev + kh) / 2)
         pw = int(((w_prev - 1) * sw - w_prev + kw) / 2)
@@ -48,11 +47,11 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     convolved = np.zeros((m, h_out, w_out, c_new))
 
     padded_A = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
-    
+
     for i in range(h_out):
         for j in range(w_out):
             for k in range(c_prev):
                 convolved[:, i, j, k] = np.sum(np.multiply(
-                    padded_A[:, i*sh: i*sh + kh, j * sw : j + kw, :],
-                     W[:, :, :, k]), axis=(1, 2, 3))
+                    padded_A[:, i*sh: i*sh + kh, j*sw: j+kw, :],
+                    W[:, :, :, k]), axis=(1, 2, 3))
     return activation(convolved + b)
