edea1adda682c377d2426c48719dbd24fcb7ce58
s-macias <1552@holbertonschool.com>
2021-11-23 23:23:26 -0500

    task 1

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+""" MOdule to create a dataframe from a dictionary """
+
+import pandas as pd
+
+dictionary = {"First": [0.0, 0.5, 1.0, 1.5],
+        "Second": ['one', "two", "three", "four"]}
+df = pd.DataFrame(dictionary, index=list("ABCD"))
