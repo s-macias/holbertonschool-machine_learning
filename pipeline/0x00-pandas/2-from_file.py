ad2f58229ce37c383343795a2f16ce439f016032
s-macias <1552@holbertonschool.com>
2021-11-23 23:42:35 -0500

    task 2

--- pipeline/0x00-pandas/2-from_file.py
+++ pipeline/0x00-pandas/2-from_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" Module to create from_file function """
+
+import pandas as pd
+
+def from_file(filename, delimiter):
+    """
+    Function that loads data from a file as a pd.DataFrame
+    Arguments:
+    filename is the file to load from
+    delimiter is the column separator
+    Returns:
+    the loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, sep=delimiter)
